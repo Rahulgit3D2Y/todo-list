e73d5a8f0a3c99b3c65f2423dd333b7b61424b51
Rahulgit3D2Y <109753648+Rahulgit3D2Y@users.noreply.github.com>
2022-11-24 16:12:49 +0530

    generate todo

---
+++ app.js
@@ -0,0 +1,40 @@
+const todoInput=document.querySelector('.todo-input');
+const todoButton=document.querySelector('.todo-button');
+const todoList=document.querySelector('.todo-list');
+
+
+todoButton.addEventListener('click',addTodo);
+
+
+
+
+
+function addTodo(event)
+{//prevent form from submitting
+   event.preventDefault();
+    
+   //todo div
+   const todoDiv=document.createElement('div');
+   todoDiv.classList.add("todo");
+   
+   //create li
+   const newTodo=document.createElement('li');
+   newTodo.innerText="hey";
+   newTodo.classList.add('todo-item');
+   todoDiv.appendChild(newTodo);
+   
+   //check mark button
+   const completeButton=document.createElement('button');
+   completeButton.innerHTML='<i class="fas fa-check"></i>';
+   completeButton.classList.add('complete-btn');
+   todoDiv.appendChild(completeButton);
+   
+   //check delete button
+   const trashButton=document.createElement('button');
+   trashButton.innerHTML='<i class="fas fa-trash"></i>';
+   trashButton.classList.add('complete-btn');
+   todoDiv.appendChild(trashButton);
+
+   //append to list
+   todoList.appendChild(todoDiv);
+}
