287ff483b3d43c891e1c477513326af7dbaead0a
Rahulgit3D2Y <109753648+Rahulgit3D2Y@users.noreply.github.com>
2022-11-25 19:41:00 +0530

    add select option

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+//const variables
 const todoInput=document.querySelector('.todo-input');
 const todoButton=document.querySelector('.todo-button');
 const todoList=document.querySelector('.todo-list');
+const filterOption=document.querySelector('.filter-todo');
 
-
+//event listeners
 todoButton.addEventListener('click',addTodo);
 todoList.addEventListener('click',deleteCheck);
+filterOption.addEventListener('click',filtertodo);
 
 
 
@@ -59,4 +62,33 @@ function deleteCheck(e){
         const todo=item.parentElement;
         todo.classList.toggle('completed');
     }
+}
+
+function filtertodo(e)
+{
+    const todos=todoList.childNodes;
+    todos.forEach(function(todo)
+    {
+        switch(e.target.value){
+            case "all":
+                todo.style.display="flex";
+                break;
+            case "completed":
+                if(todo.classList.contains("completed"))
+                {todo.style.display="flex";
+                }
+                else{
+                    todo.style.display="none";
+                }
+                break;
+            case "uncompleted":
+                if(!todo.classList.contains("completed"))
+                {todo.style.display="flex";
+                }
+                else{
+                    todo.style.display="none";
+                }
+                break;
+        }
+    });
 }
